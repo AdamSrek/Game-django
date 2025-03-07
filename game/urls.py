fde9a9a6afbcdcdfeba4836e2c07decae2dce092
Adam Šrek <D22968@oaopava.cz>
2025-03-07 08:44:06 +0100

    Delete game directory

--- game/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path  
-
-from .views import number_game
-
-urlpatterns = [
-    path('', number_game, name='number_game'),
-]
--- game/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def number_game(request):
-    return render(request, 'game/index.html')
